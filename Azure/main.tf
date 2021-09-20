867b0b2a7ab83327e329406029895bede07e987a
Josh Rolstad <jrolstad@gmail.com>
2021-09-20 12:23:17 -0700

    stubbind out needed identities

--- Azure/main.tf
+++ Azure/main.tf
@@ -125,6 +125,25 @@ resource "azurerm_cosmosdb_sql_container" "cosmoscontainer_directories" {
 }
 
 # Azure Function
+data "azuread_client_config" "current" {
+
+}
+
+resource "azuread_application" "application_api"{
+    display_name = "${var.service_name}-api-${var.environment}"
+    owners = [data.azuread_client_config.current.object_id]
+    sign_in_audience = "AzureADMyOrg"
+    web {
+        redirect_uris = ["https://localhost/.auth/login/aad/callback/"]
+    }
+}
+
+resource "azuread_service_principal" "application_sp_api" {
+    application_id = azuread_application.application_api.id
+    owners = [data.azuread_client_config.current.object_id]
+    description = "${var.service_name}-api-${var.environment}"
+}
+
 resource "azurerm_app_service_plan" "function_serviceplan" {
     name = "${var.service_name}-${var.environment}"
     resource_group_name = azurerm_resource_group.service_resource_group.name
@@ -149,5 +168,10 @@ resource "azurerm_function_app" "function_api" {
         identity_ids = [azurerm_user_assigned_identity.graph_api_managed_identity.id,azurerm_user_assigned_identity.keyvault_api_managed_identity.id]
     }
 
-    
+    auth_settings {
+      enabled = true
+      active_directory {
+        client_id = azuread_application.application_api.application_id
+      }
+    }
 }
