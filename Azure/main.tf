0f29400fc3e2bfc0c995185f882abb3d7d17e4f1
Josh Rolstad <jorolsta@microsoft.com>
2021-09-26 10:27:32 -0700

    Adding access reviews container

--- Azure/main.tf
+++ Azure/main.tf
@@ -153,6 +153,14 @@ resource "azurerm_cosmosdb_sql_container" "cosmoscontainer_directories" {
   partition_key_path  = "/Area"
 }
 
+resource "azurerm_cosmosdb_sql_container" "cosmoscontainer_accessreviews" {
+  name                = "AccessReviews"
+  resource_group_name = azurerm_cosmosdb_sql_database.cosmosdb.resource_group_name
+  account_name        = azurerm_cosmosdb_sql_database.cosmosdb.account_name
+  database_name       = azurerm_cosmosdb_sql_database.cosmosdb.name
+  partition_key_path  = "/Area"
+}
+
 # Azure Function
 resource "random_uuid" "api_user_impersonation_role" {}
 resource "random_uuid" "directory_admin_role" {}
