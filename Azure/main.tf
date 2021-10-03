2779b42a76a80d4684db82dbd5acbffb270f952c
Josh Rolstad <jorolsta@microsoft.com>
2021-10-03 15:24:35 -0700

    Adding executions

--- Azure/main.tf
+++ Azure/main.tf
@@ -190,6 +190,14 @@ resource "azurerm_cosmosdb_sql_container" "cosmoscontainer_lifecyclepolicies" {
   partition_key_path  = "/Area"
 }
 
+resource "azurerm_cosmosdb_sql_container" "cosmoscontainer_lifecyclepolicyexecutions" {
+  name                = "LifecyclePolicyExecutions"
+  resource_group_name = azurerm_cosmosdb_sql_database.cosmosdb.resource_group_name
+  account_name        = azurerm_cosmosdb_sql_database.cosmosdb.account_name
+  database_name       = azurerm_cosmosdb_sql_database.cosmosdb.name
+  partition_key_path  = "/Area"
+}
+
 # Azure Function
 resource "random_uuid" "api_user_impersonation_role" {}
 resource "random_uuid" "directory_admin_role" {}
